4d1f0418f4752fc30d11d59b2ad82feacf3bc47d
schornyi <infochyorny@gmail.com>
2016-05-10 10:07:25 +0300

    5 commit

--- Module4-solution/script.js
+++ Module4-solution/script.js
@@ -1,17 +1,13 @@
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function() {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
 
-	for (var i = 0; i < names.length; i++) {
-
-	var firstLetter = names[i].charAt(0).toLowerCase();
-
-	if (firstLetter === 'j') {
-    //console.log("Good Bye " + names[i] + "<br>");
-	document.write("Good Bye " + names[i] + "<br>"); 
-	} 
-  
-	else {
-    //console.log("Hello " + names[i] + "<br>");
-	document.write("Hello " + names[i] + "<br>"); 
-	}
-
-	}
+  if (firstLetter == "j") {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+}
+  )();
