d705083c628b26dc3d83fc6babde7249cbf553da
schornyi <infochyorny@gmail.com>
2016-05-10 13:01:49 +0300

    4.1

--- Module4-solution/SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function(window){
-var byeSpeaker = new Object();
-var speakWord = "Good Bye";
-byeSpeaker.speak = function (name) {
-  console.log(speakWord + " " + name);
-}
-
-window.byeSpeaker = byeSpeaker;
-})(window);
